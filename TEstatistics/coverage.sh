9eb254e1a6381b31c8b3d157a432df47d432463f
agustin-bilat <82325348+agustin-bilat@users.noreply.github.com>
2024-10-30 12:39:50 -0400

    Update coverage.sh

--- TEstatistics/coverage.sh
+++ TEstatistics/coverage.sh
@@ -23,7 +23,9 @@ prefixChr=$4
 function merge_ovlp_TEs_by_fam() { 
 	mkdir dir_temporal
 	cd dir_temporal
-	grep -v "Simple_repeat" ./../$1 | awk '$10 !~/rich/{print $0}' | awk '$11!~/RNA/{print}' |  awk '$10 !~/polypurine/ && NR>3 {print $5,$6-1,$7,$10,".",$9}' |awk -v OFS="\t" '$6 ~ /C/{print $1,$2,$3,$4,".","-"} $6 ~/\+/{print $1,$2,$3,$4,".",$6}' | sort -k1,1 -k2,2n > $1.bed  #convert rpmk.out to bed format and remove repeats others than TEs
+	grep -v "Simple_repeat" ./../$1 | awk '$10 !~/rich/{print $0}' | awk '$11!~/RNA/{print}' |  awk '$10 !~/polypurine/ && NR>3 {print $5,$6-1,$7,$10,".",$9}' | \
+ awk -v OFS="\t" '$6 ~ /C/{print $1,$2,$3,$4,".","-"} $6 ~/\+/{print $1,$2,$3,$4,".",$6}' | \
+ sort -k1,1 -k2,2n > $1.bed  #convert rpmk.out to bed format and remove repeats others than TEs
 	awk '{print $4}' $1.bed | sort |uniq > names.tmp
 	while read p; do awk -v var="$p" '$4==var {print $0}' $1.bed > $p.fam.bed ; done < names.tmp #split file by TE name
 	for i in [a-zA-Z]*.fam.bed ; do bedtools merge -i $i -c 4,5,6 -o distinct,distinct,distinct > $i.merge; done # bedtools merge
@@ -58,10 +60,11 @@ function calculate_feature_coverage_by_family () {
 	rm $1 TEnames_plus_ovlp.tmp
 }
 
-# Calculate total TE family size
+# Calculate total TE family coverage
 function calculate_TE_family_size() {
 grep -v "(.*)" all.merge.sort | grep -v "rich" | awk '($3-$2) >= 1 {print $4,$3-$2}' | sort -k 2,2n > $rpmk_out.bed.tmp
-awk -v g_size="$genome_size" 'NR==FNR{Arr[$1]=Arr[$1]+$2} NR!=FNR {print $1,Arr[$1],(Arr[$1]/g_size)*100}' $rpmk_out.bed.tmp TEnames.tmp >> TE_family_size   #format: TE_name |  TE_coverage(bp) | TE_coverage (%)
+awk -v g_size="$genome_size" 'NR==FNR{Arr[$1]=Arr[$1]+$2} NR!=FNR {print $1,Arr[$1],(Arr[$1]/g_size)*100}' \
+$rpmk_out.bed.tmp TEnames.tmp >> TE_family_size   #format: TE_name |  TE_coverage(bp) | TE_coverage (%)
 rm all.merge.sort $rpmk_out.bed.tmp TEnames.tmp
 }
 
@@ -82,7 +85,8 @@ calculate_feature_coverage_by_family TE_intersect  			# -> TE_genes_intersect
 calculate_TE_family_size   						# -> TE_family_size
 							#Calculate TE-family size (i.e. genomic coverage)
 
-#make a summary table
-awk -v OFS="\t" -v feature="genes" 'BEGIN{print "TE_name","TE_coverage_bp","TE_coverage_%","Overlap_with_"feature"_bp","Overlap_with_"feature"_%","Complement_Overlap_bp"} NR==FNR{Arr[$1]=$2} NR!=FNR {print $1,$2,$3,Arr[$1],(Arr[$1]*100)/$2,$2-Arr[$1]}' TE_genes_intersect TE_family_size > TE_genes_intersect.tab
+# Make the final summary table
+awk -v OFS="\t" -v feature="genes" 'BEGIN{print "TE_name","TE_coverage_bp","TE_coverage_%","Overlap_with_"feature"_bp","Overlap_with_"feature"_%","Complement_Overlap_bp"} \
+NR==FNR{Arr[$1]=$2} NR!=FNR {print $1,$2,$3,Arr[$1],(Arr[$1]*100)/$2,$2-Arr[$1]}' TE_genes_intersect TE_family_size > TE_genes_intersect.tab
 
 rm TE_genes_intersect TE_family_size
